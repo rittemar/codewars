18b48fe9b4b838b5b8b79559eee69df4ba853d45
marie-sophieritter <rittemar@oregonstate.edu>
2023-11-09 20:35:02 -0600

    Ok that last message was a lie. I learned that I have to use // rather than / for handling the large numbers

--- Number Climber2.py
+++ Number Climber2.py
@@ -4,20 +4,20 @@ def climb(n):
     # number or the double plus 1. Function climbs to N, starting at 1.
     if n == 1:
         return [1]
-    start_num = n
-    final_number = n
+    start_num = int(n)
+    final_number = int(n)
     final_lst = []
     while start_num > 1:
         if start_num % 2 != 0:
-            temp1 = (start_num - 1) / 2
+            temp1 = int((start_num - 1) // 2)
             start_num = int(temp1)
             final_lst.append(start_num)
         if start_num % 2 == 0:
-            temp1 = start_num / 2
+            temp1 = int(start_num // 2)
             start_num = int(temp1)
             final_lst.append(start_num)
     final_lst.reverse()
     final_lst.append(final_number)
     return final_lst
 
-climb(100)
+climb(373239064765307842559667748682)
