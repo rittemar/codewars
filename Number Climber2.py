7b1022281ce00cb02b4cbbdb9f5384f23d5347d7
marie-sophieritter <rittemar@oregonstate.edu>
2023-11-09 19:46:50 -0600

    4th edition.

---
+++ Number Climber2.py
@@ -0,0 +1,20 @@
+def climb(n):
+    #For every positive integer N, there exists a unique sequence starting with 1 and ending
+    # with N and such that every number in the sequence is either the double of the preceeding
+    # number or the double plus 1. Function climbs to N, starting at 1.
+    if n == 1:
+        return [1]
+    start_num = n
+    final_lst = []
+    while start_num > 1:
+        if start_num % 2 != 0:
+            temp1 = (start_num - 1) / 2
+            start_num = int(temp1)
+            final_lst.append(start_num)
+        if start_num % 2 == 0:
+            temp1 = start_num / 2
+            start_num = int(temp1)
+            final_lst.append(start_num)
+    return final_lst
+
+climb(100)
